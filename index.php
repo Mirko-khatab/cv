f2b51682784498143d3c4a868b4b5852505549dd
Mirko <mirkokawa7@gmail.com>
2021-12-31 23:14:45 +0300

    index page is ready

--- index.php
+++ index.php
@@ -14,18 +14,25 @@ include'includ/nav.php';
 ?>
 <div class="container">
 <div class="row d-flex justify-content-center mt-5">
-    <div class="col-sm-6 d-flex justify-content-end">
-<img src="img/2.svg" alt="second" width="70%">
-</div>
 <div class="col-sm-6">
-    <h1 class='text-white text-center'> Build your cv online in</h1>
+    <h1 class='text-white text-center mt-5'> Build your cv online in</h1>
     <h1 class='text-center text-white'>easiest ways</h1>
+    <br><br>
+    <p class='text-white'>
+    With “CV_ONLINE“ you can create an outstanding and professional resume in minutes.
+Create an eye-catching cover letter to send with your resume.
+Transform your resume into a mobile friendly website that you can share with recruiters!
+    </p>
+    <a class="btn w-50 text-white g" href="#!">Get started</a>
+</div>
+<div class="col-sm-6 d-flex justify-content-end mt-5">
+<img src="img/2.svg" alt="second" width="80%">
 </div>
 </div>
 </div>
 
 
-<!-- <?php
-// include'includ/footer.html';
-?> -->
+<?php
+include'includ/footer.html';
+?>
 </body>
